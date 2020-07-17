100bc699b7014462ac5e2f71d68ce0885686076f
Yancarlos <yeg003@gmail.com>
2020-07-17 17:44:45 -0400

    added footer links

--- footer.php
+++ footer.php
@@ -3,6 +3,22 @@
         <div class="footer-container">
             <div class="footer-box1">
                 <img src="<?php echo get_theme_file_uri("/images/leaf-go-logo.png");?>" alt="Footer logo">
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy.</p>
+                <div class="social-links">
+                        <a href="#">
+                            <i class="fa fa-facebook" aria-hidden="true"></i>
+                        </a>
+                        <a href="#">
+                            <i class="fa fa-instagram" aria-hidden="true"></i>
+                        </a>
+                        <a href="#">
+                            <i class="fa fa-pinterest" aria-hidden="true"></i>
+                        </a>
+                        <a href="#">
+                            <i class="fa fa-twitter" aria-hidden="true"></i>
+                        </a>
+                </div>
+
             </div>
             <div class="footer-box2">2</div>
             <div class="footer-box3">3</div>
