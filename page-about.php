3a56afb46c31a83c1e8d36563b1de0c33e4b9fbc
Yancarlos <yeg003@gmail.com>
2020-07-29 00:21:01 -0400

    added content to team box section

--- page-about.php
+++ page-about.php
@@ -44,17 +44,29 @@
         <img class ="center"src="<?php echo get_theme_file_uri( '/images/divider-img.png' )?>" alt="divider image">
         
         <div class="team-container">
-            <div class="team-box1">
-                <img src="<?php get_theme_file_uri('/images/team01.jpg' ) ?>" alt="team1">
+            <div class="team-box">
+                <img src="<?php echo get_theme_file_uri('/images/team01.jpg' ) ?>" alt="team1">
+                <h1 class="member-title">Jon Doe</h1>
+                <p class="member-sub-title">CEO & Founder</p>
+                <p class="member-text">Dolor sit amet, dolor gravida, placerat liberolorem ipsum dolor consectetur adipiscing elit, sed do eiusmod.</p>
             </div>
-            <div class="team-box2">
-                <img src="<?php get_theme_file_uri('/images/team01.jpg' ) ?>" alt="team1">
+            <div class="team-box">
+                <img src="<?php echo get_theme_file_uri('/images/team02.jpg' ) ?>" alt="team1">
+                <h1 class="member-title">Jon Doe</h1>
+                <p class="member-sub-title">CEO & Founder</p>
+                <p class="member-text">Dolor sit amet, dolor gravida, placerat liberolorem ipsum dolor consectetur adipiscing elit, sed do eiusmod.</p>
             </div>
-            <div class="team-box3">
-                <img src="<?php get_theme_file_uri('/images/team01.jpg' ) ?>" alt="team1">
+            <div class="team-box">
+                <img src="<?php echo get_theme_file_uri('/images/team03.jpg' ) ?>" alt="team1">
+                <h1 class="member-title">Jon Doe</h1>
+                <p class="member-sub-title">CEO & Founder</p>
+                <p class="member-text">Dolor sit amet, dolor gravida, placerat liberolorem ipsum dolor consectetur adipiscing elit, sed do eiusmod.</p>
             </div>
-            <div class="team-box4">
-                <img src="<?php get_theme_file_uri('/images/team01.jpg' ) ?>" alt="team1">
+            <div class="team-box">
+                <img src="<?php echo get_theme_file_uri('/images/team04.jpg' ) ?>" alt="team1">
+                <h1 class="member-title">Jon Doe</h1>
+                <p class="member-sub-title">CEO & Founder</p>
+                <p class="member-text">Dolor sit amet, dolor gravida, placerat liberolorem ipsum dolor consectetur adipiscing elit, sed do eiusmod.</p>
             </div>
         </div>
     </div>
